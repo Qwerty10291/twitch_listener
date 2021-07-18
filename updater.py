7c37950957b56f823ec917c98882eac7336fe6d5
Andrey <andrtazet@gmail.com>
2021-07-18 11:51:05 +0300

    alive time incrase

--- updater.py
+++ updater.py
@@ -4,7 +4,7 @@ from db.models import *
 from datetime import date, datetime, timedelta
 import os
 
-ALIVE_TIME = timedelta(hours=3)
+ALIVE_TIME = timedelta(hours=18)
 
 db_session.global_init()
 session = db_session.create_session()
@@ -15,8 +15,14 @@ for clip in clips:
     print(clip.time_created)
     if datetime.now() - clip.time_created > ALIVE_TIME:
         filename = clip.streamer.name + '_' + str(clip.id)
-        os.remove('static/clips/' + filename + '.mp4')
-        os.remove('static/screenshots/' + filename + '.jpg')
+        try:
+            os.remove('static/clips/' + filename + '.mp4')
+        except:
+            pass
+        try:
+            os.remove('static/screenshots/' + filename + '.jpg')
+        except:
+            pass
         clip.streamer.activity -= 1
         session.delete(clip)
 session.commit()
