a8db7a205098e7bd3db3df5283f4e1abf230fad9
Andrey <andrtazet@gmail.com>
2021-07-20 13:19:21 +0300

    fix date

--- static/js/streamers.js
+++ static/js/streamers.js
@@ -61,7 +61,7 @@ class Streamer {
     for (let clip of clip_data) {
       let container = document.createElement("div");
       let clip_date = Date.parse(clip.time_created)
-      let hours = Date.now() - clip_date / (1000 * 60 * 60)
+      let hours = (Date.now() - clip_date) / (1000 * 60 * 60)
       container.className = "video-card";
       container.innerHTML = `<img src=${clip.image}></img><span class="count">${clip.activity}</span><span class="time">${hours}</span>`;
       container.addEventListener("click", (e) => {
