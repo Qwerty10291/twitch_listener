50fc025ca695b0c0e5f26eb81e02f87ef7d00f54
Andrey <andrtazet@gmail.com>
2021-07-21 20:32:56 +0300

    js keyboard events

--- static/js/video.js
+++ static/js/video.js
@@ -7,6 +7,12 @@ document.onkeydown = (evt) => {
         case "ArrowDown":
           video_speed_normal();
           break;
+        case "Shift":
+          download_video()
+          break;
+        case "Escape":
+          window.close()
+          break;
     }
 }
 
@@ -15,4 +21,12 @@ function video_speed_up(){
 }
 function video_speed_normal() {
     document.querySelector('video').playbackRate = 1.0
+}
+function download_video(){
+    let href = document.querySelector('.download').getAttribute("href")
+    let download = document.querySelector('.download').getAttribute("download")
+    let link = document.createElement("a");
+    link.setAttribute("href", href);
+    link.setAttribute("download", download);
+    link.click();
 }
